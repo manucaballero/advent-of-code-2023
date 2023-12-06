1f8ecaa9a8f87779b3aee71500440d50530ef636
Manuel Caballero Rabago <mcabalra@mercadona.es>
2023-12-06 18:35:37 +0100

    Solved advent of code 2023 day 6 part 2

--- lib/day06/day06.dart
+++ lib/day06/day06.dart
@@ -1,7 +1,6 @@
 import 'package:advent_of_code_2023/utils/utils.dart';
 
 int partA() {
-  
   int response = 1;
   List<String> lines = obtainFileLines('lib/day06/input.txt');
   List<int> times = lines[0]
@@ -17,30 +16,65 @@ int partA() {
       .split(RegExp(r'\s+'))
       .map((e) => int.parse(e))
       .toList();
-  List<int> waysOfWinningPerRace = [];
+
+  List<int> waysOfWinningPerRace = findWaysOfWinning(times, distances);
+
+  for (int element in waysOfWinningPerRace) {
+    
+    response *= element;
+  }
+
+  return response;
+}
+
+int partB() {
+  int response = 1;
+  List<String> lines = obtainFileLines('lib/day06/input.txt');
+  List<int> times = lines[0]
+      .substring(lines[0].indexOf(":") + 1)
+      .replaceAll(" ", "")
+      .split(RegExp(r'\s+'))
+      .map((e) => int.parse(e))
+      .toList();
+
+  List<int> distances = lines[1]
+      .substring(lines[1].indexOf(":") + 1)
+      .replaceAll(" ", "")
+      .split(RegExp(r'\s+'))
+      .map((e) => int.parse(e))
+      .toList();
+
+  List<int> waysOfWinningPerRace = findWaysOfWinning(times, distances);
+
+  for (int element in waysOfWinningPerRace) {
+    
+    response *= element;
+  }
+
+  return response;
+}
+
+List<int> findWaysOfWinning(List<int> times, List<int> distances) {
   
+  List<int> waysOfWinningPerRace = [];
   for (int i = 0; i < times.length; i++) {
-    
+  
     for (int j = 1; j < times[i] - 1; j++) {
-    
+  
       int totalDistance = (times[i] - j) * j;
+  
       if (totalDistance > distances[i]) {
-    
+  
         if (i >= waysOfWinningPerRace.length) {
-    
+  
           waysOfWinningPerRace.add(1);
         } else {
-    
+  
           waysOfWinningPerRace[i] = waysOfWinningPerRace[i] + 1;
         }
       }
     }
   }
-
-  for (int element in waysOfWinningPerRace) {
-
-    response *= element;
-  }
-
-  return response;
+  
+  return waysOfWinningPerRace;
 }
