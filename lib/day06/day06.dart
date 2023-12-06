0d2a971e3e86bf123f121072ff239c6366d815a8
Manuel Caballero Rabago <mcabalra@mercadona.es>
2023-12-06 18:28:56 +0100

    Solved advent of code 2023 day 6 part 1

---
+++ lib/day06/day06.dart
@@ -0,0 +1,46 @@
+import 'package:advent_of_code_2023/utils/utils.dart';
+
+int partA() {
+  
+  int response = 1;
+  List<String> lines = obtainFileLines('lib/day06/input.txt');
+  List<int> times = lines[0]
+      .substring(lines[0].indexOf(":") + 1)
+      .trim()
+      .split(RegExp(r'\s+'))
+      .map((e) => int.parse(e))
+      .toList();
+
+  List<int> distances = lines[1]
+      .substring(lines[1].indexOf(":") + 1)
+      .trim()
+      .split(RegExp(r'\s+'))
+      .map((e) => int.parse(e))
+      .toList();
+  List<int> waysOfWinningPerRace = [];
+  
+  for (int i = 0; i < times.length; i++) {
+    
+    for (int j = 1; j < times[i] - 1; j++) {
+    
+      int totalDistance = (times[i] - j) * j;
+      if (totalDistance > distances[i]) {
+    
+        if (i >= waysOfWinningPerRace.length) {
+    
+          waysOfWinningPerRace.add(1);
+        } else {
+    
+          waysOfWinningPerRace[i] = waysOfWinningPerRace[i] + 1;
+        }
+      }
+    }
+  }
+
+  for (int element in waysOfWinningPerRace) {
+
+    response *= element;
+  }
+
+  return response;
+}
